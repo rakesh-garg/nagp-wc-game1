3809d26b8713195acf137a748d00f7f24ca8135f
Rakesh Garg <Rakesh.Garg@blackbaud.me>
2022-12-29 16:50:22 -0500

    initial check in

---
+++ build-component.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+
+build = async () =>{
+    const files = [
+        './dist/SnakeGame/runtime.js',
+        './dist/SnakeGame/polyfills.js',
+        './dist/SnakeGame/main.js'
+      ];
+
+      await fs.ensureDir('game');
+      await concat(files, 'game/snack-game.js');
+}
+
+build();
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { GameBoardComponent } from './game-board/game-board.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'SnakeGame';
+  constructor(private injector: Injector) {
+    const el = createCustomElement(GameBoardComponent, { injector });
+    customElements.define('app-game-board', el);
+  }
+  ngDoBootstrap() {}
+}
